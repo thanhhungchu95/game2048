8b44cb01cb64606ae57c4df7d42d009b279b22eb
Chu Thành Hưng <thanhhungchu95@gmail.com>
2015-08-26 07:00:33 +0700

    Update

--- Client/src/game/client/Client.java
+++ Client/src/game/client/Client.java
@@ -4,6 +4,7 @@ import game.client.ConnectForm;
 
 public class Client {
     public static void main(String[] args) {
+        // Start
         ConnectForm con = new ConnectForm();
         con.runForm();
     }
